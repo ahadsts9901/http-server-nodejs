ea00bb2259e47e4d67c77bbbfb7edba1ab38f8e0
ahadsts9901 <ahadsts990@gmail.com>
2024-07-14 06:51:10 -0700

    get-post-methods

--- server.mjs
+++ server.mjs
@@ -6,5 +6,5 @@ const server = createServer(app)
 const PORT = process.env.PORT || 5002;
 
 server.listen(PORT, () => {
-    console.log(`server running on port: ${PORT}`);
+    console.log(`server running on port ${PORT}`);
 });
