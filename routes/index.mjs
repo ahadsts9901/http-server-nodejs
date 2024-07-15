c10c2bc37f5c281397b106f5d2c17fa59baf2c76
Muhammad Ahad <120649081+ahadsts9901@users.noreply.github.com>
2024-07-15 17:02:29 +0500

    put and delete apis

--- routes/index.mjs
+++ routes/index.mjs
@@ -1,50 +1,118 @@
 // database
-const todos = []
+const todos = [];
 
 // server
 export const app = (req, res) => {
 
     res.setHeader('Content-Type', 'application/json');
 
-    switch (req.url) {
+    const urlParts = req.url.split('/');
+    const id = urlParts[urlParts.length - 1];
+    const route = `${urlParts[0]}/${urlParts[1]}/${urlParts[2]}/${urlParts[3]}`
 
+    switch (route) {
         case '/api/v1/todo':
-
             switch (req.method) {
+
+                // get
 
                 case "GET":
 
                     res.writeHead(200, { 'Content-Type': 'application/json' });
-                    res.end(JSON.stringify(
-                        {
+
+                    res.end(
+                        JSON.stringify({
                             message: 'hello world',
                             todos: todos
-                        }));
+                        })
+                    );
 
                     break;
 
+                // post
                 case "POST":
 
-                    let body = '';
+                    let postBody = '';
 
                     req.on('data', (chunk) => {
-                        body += chunk.toString(); // convert buffer to string
+                        postBody += chunk.toString(); // convert buffer to string
                     });
 
                     req.on('end', () => {
 
-                        const { todo } = JSON.parse(body);
+                        const { todo } = JSON.parse(postBody);
 
-                        todos.unshift(todo)
+                        todos.unshift(todo);
 
                         res.writeHead(200, { 'Content-Type': 'application/json' });
-                        res.end(JSON.stringify(
-                            {
+
+                        res.end(
+                            JSON.stringify({
                                 message: 'todo added',
                                 todos: todos
-                            }));
+                            })
+                        );
 
                     });
+
+                    break;
+
+                // put
+                case "PUT":
+
+                    let putBody = '';
+
+                    req.on('data', (chunk) => {
+                        putBody += chunk.toString(); // convert buffer to string
+                    });
+
+                    req.on('end', () => {
+
+                        const { todo } = JSON.parse(putBody);
+
+                        if (id !== -1) {
+
+                            todos[id] = todo;
+
+                            res.writeHead(200, { 'Content-Type': 'application/json' });
+
+                            res.end(
+                                JSON.stringify({
+                                    message: 'todo updated',
+                                    todos: todos
+                                })
+                            );
+
+                        } else {
+                            res.writeHead(404, { 'Content-Type': 'application/json' });
+                            res.end(JSON.stringify({ message: 'todo not found' }));
+                        }
+
+                    });
+
+                    break;
+
+                // delete
+                case "DELETE":
+
+                    if (id !== -1) {
+
+                        todos.splice(id, 1);
+
+                        res.writeHead(200, { 'Content-Type': 'application/json' });
+
+                        res.end(
+                            JSON.stringify({
+                                message: 'todo deleted',
+                                todos: todos
+                            })
+                        );
+
+                    } else {
+                        res.writeHead(404, { 'Content-Type': 'application/json' });
+                        res.end(JSON.stringify({ message: 'todo not found' }));
+                    }
+
                     break;
 
                 default:
@@ -61,5 +129,4 @@ export const app = (req, res) => {
             break;
 
     }
-
-}
+};
